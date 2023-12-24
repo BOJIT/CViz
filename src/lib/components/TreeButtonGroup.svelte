d1244a50de23eae027c867e565e16ca46c4cc8a2
jpedley <james.Bennion-Pedley@dyson.com>
2023-12-24 09:59:46 +0000

    Add colour picker code

--- src/lib/components/TreeButtonGroup.svelte
+++ src/lib/components/TreeButtonGroup.svelte
@@ -15,37 +15,81 @@
         RadioButtonOff,
         RadioButtonOn,
         RemoveCircle,
+        CheckmarkCircle,
     } from "@svicons/ionicons-outline";
+
+    // import ColorPicker from "svelte-awesome-color-picker";
 
     /*--------------------------------- Props --------------------------------*/
 
-    let ignore: boolean = false;
-    let blacklist: boolean = true;
+    export let ignore: boolean = true;
+    export let blacklist: boolean = true;
+
+    // export let colour: string = "#0000FF";
 
     /*-------------------------------- Methods -------------------------------*/
-
-    // TODO stop click events propogating to the tree view
 
     /*------------------------------- Lifecycle ------------------------------*/
 </script>
 
 <div class="btn-group">
-    {#if ignore}
-        <RadioButtonOn height="1rem" />
-    {:else}
-        <RadioButtonOff height="1rem" />
-    {/if}
+    <button
+        class="toggle"
+        on:click|stopPropagation={() => {
+            ignore = !ignore;
+        }}
+    >
+        {#if ignore}
+            <RadioButtonOn height="1rem" />
+        {:else}
+            <RadioButtonOff height="1rem" />
+        {/if}
+    </button>
 
-    {#if blacklist}
-        <RemoveCircle height="1rem" />
-    {:else}
-        <RadioButtonOff height="1rem" />
-    {/if}
+    <button
+        class="toggle"
+        on:click|stopPropagation={() => {
+            blacklist = !blacklist;
+        }}
+    >
+        {#if blacklist}
+            <RemoveCircle height="1rem" />
+        {:else}
+            <CheckmarkCircle height="1rem" />
+        {/if}
+    </button>
+
+    <!-- <div class="colpick">
+        <ColorPicker bind:hex={colour} isAlpha={false} label="" />
+    </div> -->
 </div>
 
 <style>
     .btn-group {
         display: flex;
         gap: 0.2rem;
+    }
+
+    .toggle {
+        border-radius: 50%;
+        z-index: 20;
+    }
+
+    .toggle:hover {
+        background-color: gray;
+    }
+
+    .colpick {
+        z-index: 30;
+    }
+
+    .colpick :global(.picker) {
+        z-index: 30;
+    }
+
+    .colpick :global(.color),
+    :global(.alpha) {
+        width: 1rem !important;
+        height: 1rem !important;
     }
 </style>
